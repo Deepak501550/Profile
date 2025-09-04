87b99b1be6eae823be686ed55a49e316267a97fb
Deepak501550 <135554459+Deepak501550@users.noreply.github.com>
2025-09-04 18:12:01 -0500

    Update app.py

--- personal_profile/app.py
+++ personal_profile/app.py
@@ -26,4 +26,4 @@ def static_files(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
